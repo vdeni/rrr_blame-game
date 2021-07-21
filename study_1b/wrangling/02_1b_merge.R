dea8e65b905823160b3f2e61dc1ee1b064940123
Denis Vlašiček <denis.vlasicek@gmail.com>
2021-07-21 16:16:50 +0200

    add renv to study 1a and 1b; 1b skeleton scripts

---
+++ study_1b/wrangling/02_1b_merge.R
@@ -0,0 +1,24 @@
+library(here)
+
+source(here::here('wrangling',
+                  '01_clean.R'))
+
+# merge to single tibble
+d_sm <- dplyr::bind_rows(d_sm_1,
+                         d_sm_2)
+
+d_prolific <- dplyr::bind_rows(d_prolific_1,
+                               d_prolific_2)
+
+d <- dplyr::full_join(d_sm,
+                      d_prolific,
+                      by = 'prolific_id')
+
+# exclude 'RETURNED' and 'TIMED OUT' participants, remove prolific id
+d %<>%
+    dplyr::filter(.,
+                  !(status %in% c('TIMED-OUT', 'RETURNED') | is.na(status)) &
+                      informed_consent == T) %>%
+    dplyr::select(.,
+                  -c('status',
+                     'prolific_id'))
