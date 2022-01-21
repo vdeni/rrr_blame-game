050d589a94e935b1c4443d08b60486ac3174e283
Denis Vlašiček <denis.vlasicek@gmail.com>
2022-01-21 12:14:55 +0100

    make for s1b; source s1b for methods

---
+++ analyses/study_1b/wrangling/study_1b_prepare-analysis-data.R
@@ -0,0 +1,28 @@
+library(here)
+library(readr)
+library(magrittr)
+library(dplyr)
+
+.fine_lim_usd <- 4500
+
+d_s1b <- readr::read_csv(here::here('analyses',
+                                    'study_1b',
+                                    'data',
+                                    'clean',
+                                    'study_1b.csv'))
+
+.d_s1b_nstart <- nrow(d_s1b)
+
+# filter out paricipants who have entered values above the exclusion criterion
+d_s1b %<>%
+    dplyr::filter(.,
+                  assess_fine < .fine_lim_usd | is.na(assess_fine))
+
+.d_s1b_ngthan <- .d_s1b_nstart - nrow(d_s1b)
+
+# filter out participants who haven't entered one of the assessments
+d_s1b %<>%
+    filter(.,
+           !is.na(assess_fine) & !is.na(assess_blame))
+
+.d_s1b_nas <- .d_s1b_nstart - .d_s1b_ngthan - nrow(d_s1b)
