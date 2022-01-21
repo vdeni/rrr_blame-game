c6e4d21ceff72c5e9b0fe510ebc3d1e6bfd390f0
Denis Vlašiček <denis.vlasicek@gmail.com>
2022-01-21 11:29:07 +0100

    start make for methods. source s1a data

---
+++ analyses/study_1a/wrangling/study_1a_prepare-analysis-data.R
@@ -0,0 +1,28 @@
+library(here)
+library(readr)
+library(magrittr)
+library(dplyr)
+
+.fine_lim_hrk <- 10200
+
+d_s1a <- readr::read_csv(here::here('analyses',
+                                    'study_1a',
+                                    'data',
+                                    'clean',
+                                    'study_1a.csv'))
+
+.d_s1a_nstart <- nrow(d_s1a)
+
+# filter out paricipants who have entered values above the exclusion criterion
+d_s1a %<>%
+    dplyr::filter(.,
+                  assess_fine < .fine_lim_hrk | is.na(assess_fine))
+
+.d_s1a_ngthan <- .d_s1a_nstart - nrow(d_s1a)
+
+# filter out participants who haven't entered one of the assessments
+d_s1a %<>%
+    filter(.,
+           !is.na(assess_fine) & !is.na(assess_blame))
+
+.d_s1a_nas <- .d_s1a_nstart - .d_s1a_ngthan - nrow(d_s1a)
